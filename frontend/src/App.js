e5eedc45b4e121206780be9eb86247b6fe56dea1
Master-GB <mastergba2001@gmail.com>
2025-03-26 19:20:30 +0530

    bug fixes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,12 +20,14 @@ import Reports from "./Components/daniruComponent/Reports";
 import Volunteers from "./Components/daniruComponent/Volunteers";
 import VolunteerCDashboard from "./Components/daniruComponent/VolunteerCDashboard";
 import UpdateTask from "./Components/daniruComponent/UpdateTask";
-import VolunteerDStaffDashboard from "./Components/daniruComponent/VolunteerDStaff/VolunteerDStaffDashboard";
-import VolunteerTask from "./Components/daniruComponent/VolunteerDStaff/VolunteerTask";
-import VolunteerTasks from "./Components/daniruComponent/VolunteerDStaff/VolunteerTasks";
 import VolunteerTaskDisplay from "./Components/daniruComponent/VolunteerDStaff/VolunteerTaskDisplay";
-import VolunteerApplication from "./Components/daniruComponent/VolunteerApplication/VolunteerApplication";
+import VolunteerTasks from "./Components/daniruComponent/VolunteerDStaff/VolunteerTasks";
 import Task from "./Components/daniruComponent/Task";
+import VolunteerDStaffDashboard from "./Components/daniruComponent/VolunteerDStaff/VolunteerDStaffDashboard";
+import VolunteerApplication from "./Components/daniruComponent/VolunteerApplication/VolunteerApplication";
+import VolunteerTask from "./Components/daniruComponent/VolunteerDStaff/VolunteerTask";
+
+
 
 const DonorLayout = () => (
   <div className="page-container">
@@ -57,43 +59,6 @@ const OperatingManagerLayout = () => (
   </div>
 );
 
-const VolunteerCoordinatorLayout = () => (
-  <div className="volunteer-coordinator-container">
-    <React.Fragment>
-      <Routes>
-        <Route path="/" element={<VolunteerCDashboard />} />
-        <Route path="/dashboard" element={<VolunteerCDashboard />} />
-        <Route path="/volunteers" element={<Volunteers />} />
-        <Route path="/createtask" element={<CreateTask />} />
-        <Route path="/volunteercdashboard" element={<VolunteerCDashboard />} />
-        <Route path="/viewtasks" element={<Task />} />
-        <Route path="/task/:id" element={<UpdateTask />} />
-        <Route path="/tracking" element={<Tracking />} />
-        <Route path="/reports" element={<Reports />} />
-      </Routes>
-    </React.Fragment>
-  </div>
-);
-
-const VolunteerDeliveryStaffLayout = () => (
-  <div className="volunteer-delivery-staff-container">
-    <React.Fragment>
-      <Routes>
-        <Route path="/" element={<VolunteerDStaffDashboard />} />
-        <Route
-          path="/volunteerdstaffdashboard"
-          element={<VolunteerDStaffDashboard />}
-        />
-        <Route path="/volunteertask" element={<VolunteerTask />} />
-        <Route path="/volunteer/:volunteerName" element={<VolunteerTask />} />
-        <Route
-          path="/volunteerapplication"
-          element={<VolunteerApplication />}
-        />
-      </Routes>
-    </React.Fragment>
-  </div>
-);
 
 function App() {
   return (
