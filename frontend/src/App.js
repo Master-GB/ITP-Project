1cd56e967a5baf369e6226301065d54666417c43
DaniruDodangoda2003 <it23246096@my.sliit.lk>
2025-03-27 11:49:12 +0530

    Updated App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -154,12 +154,24 @@ const VolunteerPackingStaffLayout = () => (
 
 function App() {
   return (
+<<<<<<< Updated upstream
       <Routes>
         <Route path="/ji/*" element={<RequestsLayout/>}/>
         <Route path="/h/*" element={<DonorLayout />} /> 
         <Route path="/ngjtrngjrn/*" element={<VolunteerCoordinatorLayout/>}/>
         <Route path="/njnjn/*" element={<VolunteerDeliveryStaffLayout/>}/>
         <Route path="/*" element={<VolunteerPackingStaffLayout/>}/>
+=======
+
+  <Routes>
+        <Route path="/lkmnk/*" element={<RequestsLayout/>}/>
+        <Route path="/jkj/*" element={<OperatingManagerLayout />} />
+        <Route path="/jh/*" element={<DonorLayout />} /> 
+       
+        <Route path="/fijrigjerig/*" element={<VolunteerCoordinatorLayout/>}/>
+        <Route path="/*" element={<VolunteerDeliveryStaffLayout/>}/>
+
+>>>>>>> Stashed changes
 
       </Routes>
   );
